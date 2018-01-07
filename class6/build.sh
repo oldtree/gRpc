9abe23b86df3649e29867027a2687a34ba8ba9ec
Grapes-tree <treeui.old@gmail.com>
2018-01-07 22:27:14 +0800

    init gRpc learning class example

---
+++ class6/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+protoc -I/usr/local/include -I. \
+		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--go_out=plugins=grpc:. \
+		class6.proto
+protoc -I/usr/local/include -I. \
+		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--grpc-gateway_out=logtostderr=true:. \
+		class6.proto
+protoc -I/usr/local/include -I. \
+		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--swagger_out=logtostderr=true:. \
+		class6.proto
+go generate .
